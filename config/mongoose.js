f7ae97090a102919134d688355d916336631396e
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2023-11-02 21:18:08 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,31 @@
-// require mongoose
+// // require mongoose
+// const mongoose = require('mongoose');
+// // connect to database and collection
+// mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+
+// const db = mongoose.connection;
+
+// // in case of any error in connection
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// // tell us once connection with database was successful
+// db.once('open', function(){
+//     console.log('Connected to Database :: MongoDB');
+// });
+
+// // exporting and make available
+// module.exports = db;
 const mongoose = require('mongoose');
-// connect to database and collection
-mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+require('dotenv').config();
 
-const db = mongoose.connection;
-
-// in case of any error in connection
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-// tell us once connection with database was successful
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
-});
-
-// exporting and make available
-module.exports = db;
+async function connectToMongoDB()
+{
+    try {
+        await mongoose.connect(process.env.mongoDBUrl);
+        console.log('database connected');
+    
+    } catch (error) {
+        console.log('error occur in conecting to db',error);
+    }
+}
+connectToMongoDB();
