e368f9c2204a897025430b7dcfaf96148803cc9f
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2023-11-04 09:11:55 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+//fill your database name here
+
+mongoose.connect(
+  'mongodb+srv://singh99vikas:Mongoose@cluster0.snh5a.mongodb.net/review_managment_system_db?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind('console', 'error'));
+
+db.once('open', function () {
+  console.log('welcome connect to database');
+});
+
+module.exports = db;
+//process.env.MONGODB_URL || 'mongodb://localhost/crawling_db',
+//Mongoose
+//'mongodb+srv://singh99vikas:Mongoose@cluster0.snh5a.mongodb.net/<crawling_db>?retryWrites=true&w=majority',
 // // require mongoose
 // const mongoose = require('mongoose');
 // // connect to database and collection
@@ -15,17 +38,19 @@
 
 // // exporting and make available
 // module.exports = db;
-const mongoose = require('mongoose');
-require('dotenv').config();
 
-async function connectToMongoDB()
-{
-    try {
-        await mongoose.connect("mongodb+srv://klsakthi333:<@Sakthi333>@cluster0.bc6vzgj.mongodb.net/");
-        console.log('database connected');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// async function connectToMongoDB()
+// {
+//     try {
+//         await mongoose.connect("mongodb+srv://klsakthi333:<@Sakthi333>@cluster0.bc6vzgj.mongodb.net/");
+//         console.log('database connected');
     
-    } catch (error) {
-        console.log('error occur in conecting to db',error);
-    }
-}
-connectToMongoDB();
+//     } catch (error) {
+//         console.log('error occur in conecting to db',error);
+//     }
+// }
+// connectToMongoDB();
+
