a883d8c8149b26819f32f6cbc022ebb78a5327d4
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2023-12-04 10:51:47 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,56 +1,56 @@
-const mongoose = require('mongoose');
-//fill your database name here
-
-mongoose.connect(
-  'mongodb+srv://klsakthi3344:klsakthi333@cluster0.b9ziqxt.mongodb.net/',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind('console', 'error'));
-
-db.once('open', function () {
-  console.log('welcome connect to database');
-});
-
-module.exports = db;
-//process.env.MONGODB_URL || 'mongodb://localhost/crawling_db',
-//Mongoose
-//'mongodb+srv://singh99vikas:Mongoose@cluster0.snh5a.mongodb.net/<crawling_db>?retryWrites=true&w=majority',
-// // require mongoose
 // const mongoose = require('mongoose');
-// // connect to database and collection
-// mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+// //fill your database name here
+
+// mongoose.connect(
+//   'mongodb+srv://klsakthi3344:klsakthi333@cluster0.b9ziqxt.mongodb.net/',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 // const db = mongoose.connection;
 
-// // in case of any error in connection
-// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+// db.on('error', console.error.bind('console', 'error'));
 
-// // tell us once connection with database was successful
-// db.once('open', function(){
-//     console.log('Connected to Database :: MongoDB');
+// db.once('open', function () {
+//   console.log('welcome connect to database');
 // });
 
-// // exporting and make available
 // module.exports = db;
+process.env.MONGODB_URL || 'mongodb://localhost/crawling_db',
+Mongoose
+'mongodb+srv://singh99vikas:Mongoose@cluster0.snh5a.mongodb.net/<crawling_db>?retryWrites=true&w=majority',
+// require mongoose
+const mongoose = require('mongoose');
+// connect to database and collection
+mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
 
-// const mongoose = require('mongoose');
-// require('dotenv').config();
+const db = mongoose.connection;
 
-// async function connectToMongoDB()
-// {
-//     try {
-//         await mongoose.connect("mongodb+srv://klsakthi333:<@Sakthi333>@cluster0.bc6vzgj.mongodb.net/");
-//         console.log('database connected');
+// in case of any error in connection
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// tell us once connection with database was successful
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+// exporting and make available
+module.exports = db;
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function connectToMongoDB()
+{
+    try {
+        await mongoose.connect("mongodb+srv://klsakthi333:<@Sakthi333>@cluster0.bc6vzgj.mongodb.net/");
+        console.log('database connected');
     
-//     } catch (error) {
-//         console.log('error occur in conecting to db',error);
-//     }
-// }
-// connectToMongoDB();
+    } catch (error) {
+        console.log('error occur in conecting to db',error);
+    }
+}
+connectToMongoDB();
 
